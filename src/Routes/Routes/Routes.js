f8db1e63efa6c49d395720d671b6e5faacb3fa5f
R7558 <rupsanaworld1004@gmail.co>
2023-05-23 20:44:33 +0600

    added navbar and footer

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,18 @@
+import Home from "../../pages/Home/Home/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../../LayOut/Main");
+
+const router=createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
+    }
+])
+export default router;
