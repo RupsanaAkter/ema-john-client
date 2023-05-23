b070e0e3c306019894984177090f3d921102c027
R7558 <rupsanaworld1004@gmail.co>
2023-05-23 22:59:20 +0600

    added update all

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-// import { addToDb, getStoredCart } from '../../utilities/fakedb'
+import { addToDb, getStoredCart } from '../../utilities/fakedb'
 import './Shop.css'
 import Products from './Products';
 import Cart from './Cart';
@@ -13,37 +13,37 @@ const Shop = () => {
         .then(data =>setProducts(data))
     }, []);
 
-    // useEffect( () =>{
-    //     const storedCart = getStoredCart();
-    //     const savedCart = [];
-    //     for(const id in storedCart){
-    //         const addedProduct = products.find(product => product.id === id);
-    //         if(addedProduct){
-    //             const quantity = storedCart[id];
-    //             addedProduct.quantity = quantity;
-    //             savedCart.push(addedProduct);
-    //         }
-    //     }
-    //     setCart(savedCart);
-    // }, [products])
+    useEffect( () =>{
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products])
 
-    // const handleAddToCart = (selectedProduct) =>{
-    //     console.log(selectedProduct);
-    //     let newCart = [];
-    //     const exists = cart.find(product => product.id === selectedProduct.id);
-    //     if(!exists){
-    //         selectedProduct.quantity = 1;
-    //         newCart = [...cart, selectedProduct];
-    //     }
-    //     else{
-    //         const rest = cart.filter(product => product.id !== selectedProduct.id);
-    //         exists.quantity = exists.quantity + 1;
-    //         newCart = [...rest, exists];
-    //     }
+    const handleAddToCart = (selectedProduct) =>{
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
         
-    //     setCart(newCart);
-    //     addToDb(selectedProduct.id);
-    // }
+        setCart(newCart);
+        addToDb(selectedProduct.id);
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -51,7 +51,7 @@ const Shop = () => {
                     products.map(product=><Products
                         key={product.id}
                         product={product}
-                        // handleAddToCart={handleAddToCart}
+                        handleAddToCart={handleAddToCart}
                         ></Products>)
                 }
             </div>
